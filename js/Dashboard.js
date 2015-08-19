4b64eebfbc876c5f258327c356438620331433db
Anmol Koul <anmol.koul@outlook.com>
2015-08-19 15:12:34 +0530

    check output
    console.log(dataSet)

--- js/Dashboard.js
+++ js/Dashboard.js
@@ -6,7 +6,7 @@ function makeGraphs(error, apiData) {
 	
 //Start Transformations
 	var dataSet = apiData;
-	console.log(dataset);
+	console.log(dataSet);
 	var dateFormat = d3.time.format("%m/%d/%Y");
 	dataSet.forEach(function(d) {
 		d.date_posted = dateFormat.parse(d.date_posted);
